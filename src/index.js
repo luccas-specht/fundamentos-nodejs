bd50a870a298eee530a6d62e7dbc8dd9867a5fe5
Luccas Specht <luccas.specht70@gmail.com>
2021-12-01 23:16:19 -0300

    feat(project): base project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/courses', (request, response) => {
+  return response.json(["curso 1", "curso 2", "curso 3"])
+  })
+
+app.post('/courses', (request, response) => {
+    return response.json(["curso 1", "curso 2", "curso 3", "curso 4"])
+  })
+
+app.put('/courses/:id', (request, response) => {
+    return response.json(["curso 6", "curso 2", "curso 3", "curso 4"])
+  })
+ 
+app.patch('/courses/:id', (request, response) => {
+    return response.json(["curso 6", "curso 7", "curso 3", "curso 4"])
+  })
+
+app.delete('/courses/:id', (request, response) => {
+    return response.json(["curso 6", "curso 7", "curso 4"])
+  })
+
+// porta na qual vai ser feita as requests
+// Ex: localhost:3333
+app.listen(3333)
+
