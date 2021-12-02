fe30b9db24080a2808a5efd6c00320144d528826
Luccas Specht <luccas.specht70@gmail.com>
2021-12-02 17:25:10 -0300

    feat(index): finished capter 1

--- src/index.js
+++ src/index.js
@@ -2,15 +2,30 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
+/*
+ -> Tipos de Paramêtros:
+    * Route Params => Identificar um recurso editar/deletar/buscar
+    * Query Params => Paginação/filtro/ordenação
+    * Body Params => Objeto para inserção/alteração (JSON) na maioria das vezes
+*/
+
 app.get('/courses', (request, response) => {
+  const query = request.query
+  console.log('querry::', query);
   return response.json(["curso 1", "curso 2", "curso 3"])
   })
 
 app.post('/courses', (request, response) => {
+  const body = request.body
+  console.log('body:', body)
     return response.json(["curso 1", "curso 2", "curso 3", "curso 4"])
   })
 
 app.put('/courses/:id', (request, response) => {
+  const params = request.params
+  console.log('params::', params);
     return response.json(["curso 6", "curso 2", "curso 3", "curso 4"])
   })
  
